2064795bf889acbb05665b79948d5b2af741f8e2
kate reeher <kreeher@four51.com>
2017-10-03 10:53:50 -0500

    removed debugging statements

--- buildTOC.py
+++ buildTOC.py
@@ -1,6 +1,8 @@
 import pathlib, os, glob, fnmatch, re, datetime, operator, calendar
 from pathlib import Path
-#import pdb
+
+# made by klreeher; auto generates repo, year, and month readmes based off of file system
+# if the month doesn't exist or the version doesn't exist, it won't get generated here
 
 monthsIndex = {0:'January', 1:'February', 2:'March', 3:'April', 4:'May', 5:'June', 6:'July', 7:'August', 8:'September', 9:'October', 10:'November', 11:'December'}
 
@@ -11,8 +13,6 @@ sortedMonths = []
 sortedVersions = []
 sortedYears = []
 
-# [[2016, April, README]]
-
 for filename in versions:
 	filename = filename.split('\\')
 	sortedList.append(filename)
@@ -22,7 +22,6 @@ for k, v in monthsIndex.items():
 	for item in sortedList:
 		if item[1] == v:
 			item[1]=k
-			#print(item[1])
 
 
 # sorts everything all nice (DOES NOT WORK IF REVERSE SORTED)
@@ -30,7 +29,6 @@ sortedList = sorted(sortedList, key=operator.itemgetter(0,1))
 
 # add the year indexes to sortedYears
 for index, item in enumerate(sortedList):
-	#print(item[0],index, item)
 	if index == 0:
 		sortedYears.append(item[0])
 	if item[0] > sortedList[index-1][0]:
@@ -38,7 +36,6 @@ for index, item in enumerate(sortedList):
 
 # add the month indexes to sortedMonths
 for index, item in enumerate(sortedList):
-	#print(index, item)
 	if index == 0:
 		sortedMonths.append([item[0], item[1]])
 	elif item[1] == 'README.md':
@@ -48,9 +45,6 @@ for index, item in enumerate(sortedList):
 
 sortedYears = sorted(sortedYears)
 sortedMonths = sorted(sortedMonths, key=operator.itemgetter(0,1))
-
-#print(sortedYears)
-#print(sortedMonths)
 
 # write the markdown for repo readme.md
 with open('README.md', 'w') as output:
@@ -81,17 +75,13 @@ for indexY, year in enumerate(sortedYears):
 	with open(''+str(year)+'/README.md', 'w') as output:
 		output.write('# OrderCloud API Releases for '+str(year)+'\n')
 		for indexM, month in enumerate(sortedMonths):
-			#print(month) 
 			if month[0] == year:
 					output.write('\n\n## ['+str(monthsIndex[month[1]])+']('+str(monthsIndex[month[1]])+'/README.md'+')')
 			else:
 					continue
 			for indexV, version in enumerate(sortedList):
-					#output.write('\n# ['+ version[0]+'](\n')
-					#output.write
 				if version[0] == year and version[1] == month[1]:
 					output.write('\n- [' + str(version[2]) + ']('+'/'+str(monthsIndex[version[1]])+'/'+str(version[2])+')')
-					#output.write('\n- [' + str(version[2]) + ']('+'/'+str(monthsIndex[version[1]])+'/'+str(version[2])+')')
 				else:
 					continue
 
@@ -100,16 +90,9 @@ for year in sortedYears:
 	for month in sortedMonths:
 		if month[0] == year:
 			with open(''+str(month[0])+'/'+str(monthsIndex[month[1]])+'/README.md', 'w') as output:
-				print(month)
 				output.write('# OrderCloud API Releases for '+str(monthsIndex[month[1]])+', '+str(month[0])+'\n')
 				for indexV, version in enumerate(sortedList):
-
-							#output.write('\n# ['+ version[0]+'](\n')
-							#output.write
 					if version[0] == year and version[1] == month[1]:
-
-						print(version)
 						output.write('\n- [' + str(version[2]) + ']('+'/'+str(version[2])+')')
-						#output.write('\n- [' + str(version[2]) + ']('+'/'+str(version[2])+')')
 					else:
 							continue
