9febee953761bc876b4cb53c1690bfa416534102
Kate Reeher <kreeher@four51.com>
2018-10-31 12:06:47 -0500

    gotta include the images as a static path

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,6 +6,8 @@ AUTHOR = 'OrderCloud'
 SITENAME = 'OrderCloud API Release Notes'
 SITEURL = 'localhost:8000'
 
+STATIC_PATHS = ['content/images']
+
 PATH = 'content'
 
 TIMEZONE = 'America/Chicago'
@@ -27,24 +29,23 @@ AUTHOR_FEED_RSS = None
 DISPLAY_CATEGORIES_ON_MENU = True
 
 LINKS = (
-	       ('OrderCloud.io', 'https://ordercloud.io'),
-         ('OrderCloud Documentation', 'https://documentation.ordercloud.io/'),
-                  ('OrderCloud.io Community Support Slack', 'http://community.ordercloud.io/')
-         )
+    ('OrderCloud.io', 'https://ordercloud.io'),
+    ('OrderCloud Documentation', 'https://documentation.ordercloud.io/'),
+    ('OrderCloud.io Community Support Slack', 'http://community.ordercloud.io/')
+)
 
 
 # Social widget
 TWITTER = ('OrderCloud.io Twitter', 'https://twitter.com/ordercloudapi')
-RSS_FEED = ('Full Site RSS', SITEURL+'/release-notes/feeds/all.atom.xml')
-CAT_RSS_FEED = ('Release Notes RSS', SITEURL+'/release-notes/feeds/release-notes.atom.xml')
+RSS_FEED = ('Full Site RSS', SITEURL + '/release-notes/feeds/all.atom.xml')
+CAT_RSS_FEED = ('Release Notes RSS', SITEURL +
+                '/release-notes/feeds/release-notes.atom.xml')
 
 SOCIAL = (
-          TWITTER,
-          RSS_FEED,
-          CAT_RSS_FEED
-         )
-
-
+    TWITTER,
+    RSS_FEED,
+    CAT_RSS_FEED
+)
 
 
 # plugins
@@ -52,21 +53,21 @@ SOCIAL = (
 PLUGIN_PATHS = ['pelican-plugins', 'pelican-bootstrapify', 'plugins']
 
 PLUGINS = {
-					'assets',
-					'sitemap',
-					'touch',
-          'pelican-bootstrapify',
-          'neighbors',
-          'representative_image',
-          'tag_cloud',
-          'tipue_search',
-          'i18n_subsites'
-          #'post-stats',
-          #'yuicompressor' #enable after theming
+    'assets',
+    'sitemap',
+    'touch',
+    'pelican-bootstrapify',
+    'neighbors',
+    'representative_image',
+    'tag_cloud',
+    'tipue_search',
+    'i18n_subsites'
+    #'post-stats',
+    #'yuicompressor' #enable after theming
 
-					#'filetime_from_git'
-					#'ace_editor'
-					}
+    #'filetime_from_git'
+    #'ace_editor'
+}
 
 DEFAULT_DATE = 'fs'
 
@@ -75,7 +76,7 @@ TYPOGRIFY = True
 SITEMAP = {
     'format': 'xml',
     'priorities': {
-    'articles': 0.5,
+        'articles': 0.5,
         'indexes': 0.5,
         'pages': 0.5
     },
@@ -99,7 +100,7 @@ RELATIVE_URLS = True
 THEME = "pelican-themes/pelican-bootstrap3"
 
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
-SHOW_ARTICLE_AUTHOR =  False
+SHOW_ARTICLE_AUTHOR = False
 SHOW_ARTICLE_CATEGORY = True
 SHOW_DATE_MODIFIED = True
 DISPLAY_TAGS_ON_SIDEBAR = False
@@ -110,16 +111,12 @@ BOOTSTRAP_THEME = 'lumen'
 DISPLAY_ARCHIVE_ON_SIDEBAR = True
 BOOTSTRAP_NAVBAR_INVERSE = True
 SEARCH_URL = 'search.html'
-#FAVICON = 
+# FAVICON =
 DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'search')
 
 GOOGLE_ANALYTICS_UNIVERSAL = 'UA-82258138-2'
 
 
-
-
 DEFAULT_PAGINATION = 5
 YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
 MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
-
-
